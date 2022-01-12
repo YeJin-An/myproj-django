414d8e05776915ede20d9ae1ea318b5d82309d52
안예진 <ahw24033@naver.com>
2022-01-12 10:24:04 +0900

    news admin 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import PostViewSet
@@ -10,14 +9,4 @@ router.register("posts", PostViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
-=======
-from django.urls.resolvers import URLPattern
-
-
-
-from django.urls import path, include
-
-urlpatterns = [
-
->>>>>>> 857150f6928d235e42023773913c9a9a034aa261
 ]
