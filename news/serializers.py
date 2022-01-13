d5ed3fe1d09de67e0d7a851cf6b80b0e629d7bc1
안예진 <ahw24033@naver.com>
2022-01-13 16:23:11 +0900

    rest_framework_generics 캡슐화??

--- news/serializers.py
+++ news/serializers.py
@@ -4,4 +4,27 @@ from news.models import News
 class NewsSerializer(serializers.ModelSerializer):
   class Meta:
     model = News
-    fields = "__all__"
+    fields = ["id","title","content"]
+
+  def validate_title(self, title):
+      if len(title)<3:
+        raise serializers.ValidationError("3글자 이상!!")
+      return title
+
+# # 비로그인 사용자용
+# class NewsAnonymousSerializer(serializers.ModelSerializer):
+#   class Meta:
+#     model = News
+#     fields = ["id","title","content"]
+
+# # 골드맵버쉽 사용자용
+# class NewsGoldSerializer(serializers.ModelSerializer):
+#   class Meta:
+#     model = News
+#     fields = ["id","title","content","photo"]
+
+# # 관리자용
+# class NewsAdminSerializer(serializers.ModelSerializer):
+#   class Meta:
+#     model = News
+#     fields = ["id","title","content","photo","created_at","updated_at"]
--- news/views.py
+++ news/views.py
@@ -1,19 +1,35 @@
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
 from news.models import News
-from blog.serializers import PostSerializer
+from blog.serializers import PostSerializer ,NewsAdminSerializer
 
 import json
 from django.http import HttpRequest, HttpResponse
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
-    serializer_class = PostSerializer
+    # serializer_class = PostSerializer
+    def get_serializer_class(self):
+        return NewsAdminSerializer
+        # 함수의 이름을 넣어주는 역활??
 
-article_list = ListAPIView.as_view(
-    queryset = News.objects.all(),
-    serializer_class = PostSerializer
-)
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.query_params.get("query","")
+        if query:
+            qs = qs.filter(title_ic=query)
+        year = self.request.query_params.get("year","")
+        if year:
+            qs = qs.filter(title_ic=year)
+        return qs
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
+
+# article_list = ListAPIView.as_view(
+#     queryset = News.objects.all(),
+#     serializer_class = PostSerializer
+# )
 
 # def Article_list(request):
 #     qs = News.objects.alll()
