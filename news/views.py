c55a2ca58b4f732f50fb4339ad38f5b7fc18254a
안예진 <ahw24033@naver.com>
2022-01-13 15:27:10 +0900

    줄이기 끝

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from news.models import News
 from blog.serializers import PostSerializer
 
@@ -9,16 +10,22 @@ class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = PostSerializer
 
+article_list = ListAPIView.as_view(
+    queryset = News.objects.all(),
+    serializer_class = PostSerializer
+)
 
-def Article_list(request):
-    qs = News.objects.alll()
-    dta = [
-        {"id":article.id, 
-        "title":article.title, 
-        "content":article.content,
-        "photo": request.build_absolute_url(article.photo.url) if article.photo else None,
-        }
-        for article in qs
-    ]
-    json_string = json.dumps(data)
-    return HttpResponse(json_string)
+# def Article_list(request):
+#     qs = News.objects.alll()
+#     serializer = PostSerializer(qs,many=True)
+#     data = serializer.data
+#     # data = [
+#     #     {"id":article.id, 
+#     #     "title":article.title, 
+#     #     "content":article.content,
+#     #     "photo": request.build_absolute_url(article.photo.url) if article.photo else None,
+#     #     }
+#     #     for article in qs
+#     # ]
+#     json_string = json.dumps(data)
+#     return HttpResponse(json_string)
