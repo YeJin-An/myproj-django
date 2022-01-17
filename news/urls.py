3203552c137f0ccd673762cf03b9acb28d6919f0
안예진 <ahw24033@naver.com>
2022-01-17 13:34:39 +0900

    djangorestframwork-simplejwt

--- news/urls.py
+++ news/urls.py
@@ -9,3 +9,15 @@ urlpatterns = [
   path("articles.json", news.article_list),
   path("api/", include(router.urls)),
 ]
+
+# TOCO 아래 token view는 accounts앱 ㅐ에 두는 것이 맞습니다.
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns += [
+  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
